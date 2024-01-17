e12c01ed59cb80f57873039c944524f97c01d2e3
sjsafranek <sjsafranek@gmail.com>
2024-01-17 15:18:27 -0800

    request logging

--- services/weather/endpoints.py
+++ services/weather/endpoints.py
@@ -15,7 +15,7 @@ router = APIRouter()
 
 @router.post('/find', response_class=JSONResponse)
 async def post_find(params: models.WeatherParams):
-	print(json.dumps(params))
+	print(params.longitude, params.latitude, params.radius, params.precision)
 	gdf = OpenWeatherApi.getWeatherAroundPoint(Point(params.longitude, params.latitude), radius=params.radius, precision=params.precision)
 	return json.loads(gdf.to_json())
 
